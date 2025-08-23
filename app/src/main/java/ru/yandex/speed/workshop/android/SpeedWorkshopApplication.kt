4405dd95db72c30548ea1ea77f120a588c2a1732
daniilshlapak <daniilshlapak@yandex-team.ru>
2025-08-23 10:31:49 +0300

    Блокирующая аналитика на старте

--- app/src/main/java/ru/yandex/speed/workshop/android/SpeedWorkshopApplication.kt
+++ app/src/main/java/ru/yandex/speed/workshop/android/SpeedWorkshopApplication.kt
@@ -3,6 +3,10 @@ package ru.yandex.speed.workshop.android
 import android.app.Application
 import com.yandex.analytics.api.YandexAnalytics
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import ru.yandex.speed.workshop.android.data.network.NetworkPrewarmer
 import ru.yandex.speed.workshop.android.utils.ApplicationStartupTracker
 import ru.yandex.speed.workshop.android.utils.PerformanceTimestamp
@@ -16,6 +20,8 @@ import javax.inject.Inject
 class SpeedWorkshopApplication : Application() {
     @Inject
     lateinit var startupTracker: ApplicationStartupTracker
+    
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
     override fun onCreate() {
         // Инициализация метки времени старта приложения
@@ -30,7 +36,11 @@ class SpeedWorkshopApplication : Application() {
         // Инициализация Timber для логирования
         Timber.plant(Timber.DebugTree())
 
-        YandexAnalytics.getInstance().initialize(this, "demo-api-key-12345")
+        // Инициализация YandexAnalytics в фоновом потоке для избежания блокировки main thread
+        applicationScope.launch(Dispatchers.IO) {
+            YandexAnalytics.getInstance().initialize(this@SpeedWorkshopApplication, "demo-api-key-12345")
+            Timber.d("YandexAnalytics initialized in background")
+        }
 
         // Инициализация трекера запуска приложения
         startupTracker.initialize(this)
